4e74e74439bafa9609875c62d6b93707f94e13d2
yeachan0305 <103321805+yeachan0305@users.noreply.github.com>
2022-05-30 17:15:47 +0900

    Update price.py

--- price.py
+++ price.py
@@ -1,1 +1,18 @@
-h = 1
+def shopping(shop_file):
+    shop_dict = {} # 생성할 사전 객체
+    
+    with open(f'data/{shop_file}') as f:
+        cnt = 0
+        for line in f:
+            if cnt == 0 or cnt == 1:
+                cnt += 1
+            else:
+                A, B = line.split()
+                shop_dict[A] = B[:-1]
+
+    return shop_dict
+  
+def item_price(shop_file, item):
+    
+    shoplist = shopping(shop_file)
+    return shoplist[item]
